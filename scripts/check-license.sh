e6b86918d0675bc102e8fed2d350f49b2da80a28
saimu.msm <saimu.msm@antfin.com>
2023-04-04 23:39:35 +0800

    build project

---
+++ scripts/check-license.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# usage: add license for all java sources
+
+script_dir=`dirname $0`
+project_root=`realpath $script_dir/..`
+
+cd $project_root
+
+mvn -T 1C -f pom.xml license:check
